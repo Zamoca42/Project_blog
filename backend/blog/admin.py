f0f9454e35d30c60115a50686ee873d492269d65
Zamoca <suntail2002@naver.com>
2022-07-14 21:04:30 +0900

    update admin page

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post, Category, Tag
 
 # Register your models here.
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["photo_tag", "title", "category_set"]
+    list_dislay_links = ["title"]
+    
+    def photo_tag(self, post):
+        return mark_safe(f"<img src={post.photo.url} width=25% />")
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
