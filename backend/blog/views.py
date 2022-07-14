5f4765b4ee7ec4d63ebef73e5c87b2b55a912ff3
Zamoca <suntail2002@naver.com>
2022-07-14 21:06:35 +0900

    add viewpage

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly] # FIXME: 인증 적용
+
