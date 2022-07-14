c8f714e846d1924cfaf59ff514f7ce14e6fac6d3
Zamoca <suntail2002@naver.com>
2022-07-14 21:05:35 +0900

    update serializers

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,3 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+from . import views
+
+router = DefaultRouter()
+router.register('posts', views.PostViewSet)
+
+
 urlpatterns = [
-    
+    path('api/', include(router.urls)),
 ]
